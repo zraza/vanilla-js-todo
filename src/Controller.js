9de03f91311b24d4843db8e699a94e8f2693ab1a
Zeeshan Raza <zeeshanraza@MacBook-Pro.local>
2016-09-26 20:41:45 +0100

    first commit

---
+++ src/Controller.js
@@ -0,0 +1,48 @@
+/*global PubSub */
+
+(function(window) {
+    'use strict';
+
+    /*
+     * Typical controller, wiring up things
+     */
+
+    var model, view;
+
+    function init(modelObj, viewObj) {
+        model = modelObj;
+        view = viewObj;
+
+        view.bind('todoNew', onAddNew);
+        view.bind('todoAdd', onAddNew);
+        view.bind('todoDelete', onDelete);
+
+        PubSub.subscribe('TodoListChanged', onListChange)
+
+
+    }
+
+    function onAddNew(title) {
+        if (title.trim() === '') {
+            return;
+        }
+
+        model.add({ title: title });
+    }
+
+    function onDelete(id) {
+        model.remove(id);
+    }
+
+    function onListChange(items) {
+        view.render(items);
+    }
+
+    var Controller = {
+        init: init
+    };
+
+
+    window.app = window.app || {};
+    window.app.Controller = Controller;
+})(window);
---
+++ src/PubSub.js
@@ -0,0 +1,47 @@
+(function(window) {
+    'use strict';
+
+    /*
+     * Implementation of simple Observer pattern
+     */
+
+    var subscribers = {};
+
+    function subscribe(subscriberName, fn) {
+        subscribers[subscriberName] = subscribers[subscriberName] || [];
+        subscribers[subscriberName].push(fn);
+    }
+
+    function unSubscribe(subscriberName, fn) {
+        if (subscribers[subscriberName]) {
+            for (var i = 0; i < subscribers[subscriberName].length; i++) {
+                if (subscribers[subscriberName][i] === fn) {
+                    subscribers[subscriberName].splice(i, 1);
+                    break;
+                }
+            }
+        }
+    }
+
+    function publish(subscriberName, data) {
+        if (subscribers[subscriberName]) {
+            subscribers[subscriberName].forEach(function(fn) {
+                fn(data);
+            });
+        }
+    }
+
+    /**
+     * Export the public methods
+     */
+
+    var PubSub = {
+        subscribe: subscribe,
+        unSubscribe: unSubscribe,
+        publish: publish
+    };
+
+
+    window.PubSub = PubSub;
+
+})(window);
